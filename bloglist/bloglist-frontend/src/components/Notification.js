5b06ed7d497ba2411d8e2f6ff578433e4c77c157
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-01-04 12:10:40 -0800

    Separated reducer state concern in notification via useSelector

--- bloglist/bloglist-frontend/src/components/Notification.js
+++ bloglist/bloglist-frontend/src/components/Notification.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 
 const Notification = () => {
 
-  const notification = useSelector(state => state)
+  const notification = useSelector(state => state.notification)
   
   if ( !notification ) {
     return null
