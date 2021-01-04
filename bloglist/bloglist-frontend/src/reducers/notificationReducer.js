c0ef5c33fdfae98563863ebcd17872f99a3922bc
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-01-03 17:13:37 -0800

    Built notification reducer

--- bloglist/bloglist-frontend/src/reducers/notificationReducer.js
+++ bloglist/bloglist-frontend/src/reducers/notificationReducer.js
@@ -1,5 +1,17 @@
 const reducer = (state = [], action) => {
+// Okay, in here, I need to come up with a switch statement that allows for setting
+// the notification, and another for clearing the notification
     console.log('dummy')
+    switch(action.type) {
+        case 'SET_NOTIFICATION':
+            return action.data
+        case 'CLEAR_NOTIFICATION':
+            return null
+        default: 
+            return state
+    }
 }
+
+// action creators 
 
 export default reducer
