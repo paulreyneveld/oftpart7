57fe848391114d0d766408175b0ee94bed3ec4b7
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-01-10 17:29:44 -0800

    Refactored Login to its own component

--- bloglist/bloglist-frontend/src/App.js
+++ bloglist/bloglist-frontend/src/App.js
@@ -5,6 +5,7 @@ import loginService from './services/login'
 import BlogForm from './components/BlogForm'
 import Togglable from './components/Togglable'
 import Notification from './components/Notification'
+import Login from './components/Login'
 import { setNotification } from './reducers/notificationReducer'
 import { useDispatch, useSelector } from 'react-redux'
 import { initializeBlogs, newBlog, incrementLike } from './reducers/blogReducer'
@@ -19,9 +20,6 @@ const App = () => {
     return state.login
   })
   console.log(userInfo)
-
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
   
   const blogFormRef = useRef()
 
@@ -39,34 +37,6 @@ const App = () => {
       blogService.setToken(user.token)
     }
   }, [dispatch])
-
-  const handleLogin = async (event) => {
-    event.preventDefault()
-
-    try {
-      const user = await loginService.login({
-        username, password,
-      })
-
-      window.localStorage.setItem(
-        'loggedBlogappUser', JSON.stringify(user)
-      )
-      blogService.setToken(user.token)
-      dispatch(initializeUser(user))
-      setUsername('')
-      setPassword('')
-      dispatch(setNotification({
-        message: `${user.name} welcome back`,
-        type: 'success'
-      }, 3))
-    } catch (exception) {
-      console.log(exception)
-      dispatch(setNotification({
-        message: `wrong username/password`,
-        type: 'error'
-      }, 3))
-    }
-  }
 
   const handleLogout = () => {
     window.localStorage.removeItem('loggedBlogappUser')
@@ -86,38 +56,41 @@ const App = () => {
     dispatch(incrementLike(newBlog))
   }
 
-  if (!userInfo.loggedIn) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <Notification />
-        <form onSubmit={handleLogin}>
-          <div>
-            username 
-              <input
-              type="text"
-              id="username"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-              />
-          </div>
-          <div>
-            password 
-            <input
-            type="password"
-            id="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit" id="login-button">login</button>
-        </form>
-      </div>
-    )
-  }
+  // if (!userInfo.loggedIn) {
+  //   return (
+  //     <div>
+  //       <h2>Log in to application</h2>
+  //       <Notification />
+  //       <form onSubmit={handleLogin}>
+  //         <div>
+  //           username 
+  //             <input
+  //             type="text"
+  //             id="username"
+  //             value={username}
+  //             name="Username"
+  //             onChange={({ target }) => setUsername(target.value)}
+  //             />
+  //         </div>
+  //         <div>
+  //           password 
+  //           <input
+  //           type="password"
+  //           id="password"
+  //           value={password}
+  //           name="Password"
+  //           onChange={({ target }) => setPassword(target.value)}
+  //           />
+  //         </div>
+  //         <button type="submit" id="login-button">login</button>
+  //       </form>
+  //     </div>
+  //   )
+  // }
 
+  if (!userInfo.loggedIn) {
+    return <Login />
+  }
   if (!blogs) {
     return null
   }
@@ -125,7 +98,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{userInfo.user.name} is logged in</p>
+      {/* <p>{userInfo.user.name} is logged in</p> */}
       <Notification />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} updateBlogLikes={updateBlogLikes} />
---
+++ bloglist/bloglist-frontend/src/components/Login.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import loginService from '../services/login'
+import blogService from '../services/blogs'
+import { useDispatch } from 'react-redux'
+import { initializeUser } from '../reducers/loginReducer'
+import { setNotification } from '../reducers/notificationReducer'
+import Notification from '../components/Notification'
+
+
+const Login = () => {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const dispatch = useDispatch()
+
+    const handleLogin = async (event) => {
+      event.preventDefault()
+
+      try {
+        const user = await loginService.login({
+          username, password,
+        })
+
+        window.localStorage.setItem(
+          'loggedBlogappUser', JSON.stringify(user)
+        )
+        blogService.setToken(user.token)
+        dispatch(initializeUser(user))
+        setUsername('')
+        setPassword('')
+        dispatch(setNotification({
+          message: `${user.name} welcome back`,
+          type: 'success'
+        }, 3))
+      } catch (exception) {
+        console.log(exception)
+        dispatch(setNotification({
+          message: `wrong username/password`,
+          type: 'error'
+        }, 3))
+      }
+    }
+
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <Notification />
+        <form onSubmit={handleLogin}>
+          <div>
+            username 
+              <input
+              type="text"
+              id="username"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+              />
+          </div>
+          <div>
+            password 
+            <input
+            type="password"
+            id="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit" id="login-button">login</button>
+        </form>
+      </div>
+    )
+}
+
+export default Login 
