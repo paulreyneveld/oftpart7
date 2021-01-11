127583bf2d12bbd6ce147da2d8b1549063e7991c
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-01-11 14:53:46 -0800

    Added logout button to Users view

--- bloglist/bloglist-frontend/src/components/Users.js
+++ bloglist/bloglist-frontend/src/components/Users.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-const Users = () => {
+const Users = ({ handleLogout }) => {
 
     const users = useSelector(state => state.users)
     const loggedIn = useSelector(state => state.login.user.name)
@@ -11,6 +11,7 @@ const Users = () => {
         <>
         <h2>blogs</h2>
         <p>{loggedIn} is logged in.</p>
+        <button onClick={handleLogout}>Logout</button>
         <h2>users</h2>
         {users.map(user => 
             <p key={user.id}>User: {user.name} <br />Blogs Written: {user.blogs.length}</p>
