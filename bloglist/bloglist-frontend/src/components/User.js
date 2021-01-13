d4969467d78563aed998492b18450ac4089a79a1
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-01-13 11:22:37 -0800

    Obtained user list from store

--- bloglist/bloglist-frontend/src/components/User.js
+++ bloglist/bloglist-frontend/src/components/User.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const User = (props) => {
     console.log(props.match.params.userId)
+    const user = useSelector(state => state.users)
+    console.log(user)
     return <h1>Hello world</h1>
 }
 
