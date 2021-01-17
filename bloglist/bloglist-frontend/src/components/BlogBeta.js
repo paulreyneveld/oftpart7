fdde0da75b37b19b11484f09de39957b91195a56
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-01-17 12:19:42 -0800

    Added title, url, and author to blog view

--- bloglist/bloglist-frontend/src/components/BlogBeta.js
+++ bloglist/bloglist-frontend/src/components/BlogBeta.js
@@ -1,17 +1,38 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-const BlogBeta = ({blogs, updateBlogLikes, handleLogout}) => {
-
-    console.log('hello')
+const BlogBeta = (props, { updateBlogLikes, handleLogout }) => {
+    const targetId = props.match.params.blogId
     const loggedIn = useSelector(state => state.login.user.name)
-    console.log(loggedIn)
+    const blog = useSelector(state => state.blogs.filter(blog =>    
+        blog.id === targetId
+    ))
+
+    console.log(blog)
+    const addLikes = (event) => {
+        event.preventDefault()
+        updateBlogLikes({
+          user: blog.user,
+          title: blog.title,
+          author: blog.author,
+          url: blog.url,
+          likes: blog.likes + 1,
+          id: blog.id
+        })
+      }
+
+    if (blog.length < 1) {
+        return null
+    }
 
     return (
         <>
         <h2>blogs</h2>
         <p>{loggedIn} logged in </p>
         <button onClick={handleLogout}>Logout</button>
+        <h2>{blog[0].title}</h2>
+        <p><a href={`http://${blog[0].url}`}>{blog[0].url}</a></p>
+        <p>added by {blog[0].author}</p>
         </>
     )
 }
