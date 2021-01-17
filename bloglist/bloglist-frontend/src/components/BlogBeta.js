32e51d3bedc297f5c144f2a9ce784e480198410a
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-01-17 12:06:29 -0800

    Added Logout to blog view

--- bloglist/bloglist-frontend/src/components/BlogBeta.js
+++ bloglist/bloglist-frontend/src/components/BlogBeta.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const BlogBeta = ({blogs, updateBlogLikes, handleLogout}) => {
+
+    console.log('hello')
+    const loggedIn = useSelector(state => state.login.user.name)
+    console.log(loggedIn)
+
     return (
         <>
         <h2>blogs</h2>
-        <p>... logged in </p>
+        <p>{loggedIn} logged in </p>
+        <button onClick={handleLogout}>Logout</button>
         </>
     )
 }
